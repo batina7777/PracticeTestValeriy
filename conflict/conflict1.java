596ea9814f82eeb558b84e06031d3289d57c3029
Alexandra <161351738+alexameow@users.noreply.github.com>
2024-04-27 13:11:12 -0400

    last commit

--- conflict/conflict1.java
+++ conflict/conflict1.java
@@ -4,7 +4,16 @@ public class conflict1 {
     //inf
     //,,,,
     //....
+    //
+    // doing new conflict
 
     // i need a new computer
+
+
+    // 1 some new information
+
+
+    // 2 some new information
+
 
 }
